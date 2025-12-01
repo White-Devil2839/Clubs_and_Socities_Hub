2ffa0ee356844f74909fed0216b6e50cfa408eac
Neha Sharma <neha.k@adypu.edu.in>
2025-12-01 23:53:08 +0530

    Format userRoutes.js for consistency

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { getMyMemberships, getMyEventRegistrations } = require('../controllers/userController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 
-
 router.get('/memberships', authMiddleware, getMyMemberships);
 router.get('/event-registrations', authMiddleware, getMyEventRegistrations);
 
